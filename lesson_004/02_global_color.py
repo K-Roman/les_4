5368a116e9930f132e7d829a5b3b927026502a3f
K-Roman <cawme@mail.ru>
2020-05-15 17:32:28 +0300

    решено

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,6 +14,46 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+color_dict = {0:'COLOR_RED', 1:'COLOR_ORANGE', 2:'COLOR_YELLOW',
+              3:'COLOR_GREEN', 4:'COLOR_CYAN', 5:'COLOR_BLUE', 6:'COLOR_PURPLE'}
+
+
+def fig(point, length, angle, angle1):
+    for i, name in color_dict.items():
+        print(i, name)
+    color = int(input("Выбери из цвет списка  "))
+
+    for angle in range(angle, angle + 359, angle1):
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+        c1=color_dict[color]
+        print(c1)
+        v1.draw(color=sd.c1)
+        point=v1.end_point
+
+def treeangle(point = sd.get_point(300, 300), length = 100, angle = 0):
+    fig(point=point, length=length, angle=angle, angle1=120)
+    l1=sd.line(point,point)
+
+
+# - квадрата
+def quadro(point = sd.get_point(300, 300), length = 100, angle = 0):
+    fig(point=point, length=length, angle=angle, angle1=90)
+
+# # - пятиугольника
+def fangle(point = sd.get_point(100, 300), length = 100, angle = 0):
+    fig(point=point, length=length, angle=angle, angle1=72)
+
+# # - шестиугольника
+
+def sangle(point = sd.get_point(300, 300), length = 100, angle = 0):
+    fig(point=point, length=length, angle=angle, angle1=60)
+
+treeangle(point = sd.get_point(100, 100), length = 500, angle = 30)
+# quadro(point = sd.get_point(400, 100), length = 200, angle = 45)
+# fangle(point = sd.get_point(600, 50), length = 100, angle = 0)
+# sangle(point = sd.get_point(200, 300), length = 100, angle = 0)
 
 sd.pause()
--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,25 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+
+sd.resolution = (1200, 600)
+
+form_dict = {0:'треугольник', 1:'квадрат', 2:'пятиугольник',
+              3:'шестиугольник'}
+
+def fig(point, length, angle):
+    for i, name in form_dict.items():
+        print(i, name)
+    shape = int(input('введите желаемую форму   '))
+    angles = {0:120, 1:90, 2:72, 3:60}
+    for angle in range(angle, angle + 359, angle1 = int(angles[shape])):
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+        v1.draw()
+        point=v1.end_point
+
+fig(point = sd.get_point(100, 100), length = 500, angle = 30)
+
+
+
 
 sd.pause()
