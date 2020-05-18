91542c68d9a9bc78bdf5400796b87851adf932ab
K-Roman <cawme@mail.ru>
2020-05-18 17:38:56 +0300

    решено

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import random
 
+n=0
+list_x=[]
+list_y=[]
+list_l=[]
+while n<100:
+    list_x.append(random.randint(50,1100))
+    list_y.append(random.randint(250,600))
+    list_l.append(random.randint(10, 25))
+    n = n + 1
+sd.resolution = (1200, 600)
 
+print(len(list_x))
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+
 
 # Пригодятся функции
 # sd.get_point()
@@ -18,14 +30,38 @@ N = 20
 # sd.user_want_exit()
 
 # TODO здесь ваш код
+
+
+# for i in list_x:
+#     y = random.choice(list_y)
+#     x = random.choice(list_x)
+#     length = sd.random_number(10, 101
+
+for i, item in enumerate(list_x):
+    x = list_x[i]
+    y = list_y[i]
+    point_0 = sd.get_point(x, y)
+    length = list_l[i]
+    sd.snowflake(center=point_0, length=length)
+
 while True:
+    for i, item in enumerate(list_y):
+        a = sd.random_number(-25, 25)
+        x = list_x[i] + a
+        y = list_y[i] - 10
+        point_0 = sd.get_point(x, y)
+        length = list_l[i]
+        sd.snowflake(center=point_0, length=length)
+        list_y[i]=list_y[i] - 10
+        if y < 20:
+            list_y[i]=random.choice(list_y)
+
+    sd.sleep(0.2)
     sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+
+
+
 
 sd.pause()
 
